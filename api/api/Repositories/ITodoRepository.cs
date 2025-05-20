21fd1ec84bb048a1ae147f3033f0daec210ceca5
Sahina-Parwin <parweensahina89@gmail.com>
2025-05-20 12:29:46 +0530

    Todo Repository Added

--- api/api/Repositories/ITodoRepository.cs
+++ api/api/Repositories/ITodoRepository.cs
@@ -5,5 +5,12 @@ namespace api.Repositories
     public interface ITodoRepository 
     {
         public List<Todoitem> GetAll();
+
+        public Todoitem GetById(int id);
+
+        public Todoitem Create(Todoitem item);
+
+        public Todoitem Update(Todoitem item);
+        public Todoitem Delete(int id);
     }
 }
--- api/api/Repositories/TodoRepository.cs
+++ api/api/Repositories/TodoRepository.cs
@@ -10,9 +10,65 @@ namespace api.Repositories
         {
             this.context = context;
         }
+
+        public Todoitem Create(Todoitem item)
+        {
+            // create row
+            context.Add(item);
+            // or context.Todoitems.Add(item);
+
+
+            // Save changes
+            context.SaveChanges();
+            // Return new row
+
+            return item;
+        }
+
+        public Todoitem Delete(int id)
+        {
+          // fetch item 
+          Todoitem item = context.Todoitems.Where(src  => src.Id == id).FirstOrDefault();
+            if (item == null)
+            {
+                throw new Exception("item not found");
+            }
+          // delete item
+          context.Todoitems.Remove(item);
+            // save changes 
+            context.SaveChanges();
+            // return
+            return item;
+        }
+
         public List<Todoitem> GetAll()
         {
             return context.Todoitems.ToList();
+        }
+
+        public Todoitem GetById(int id)
+        {
+            // fetch item 
+            Todoitem item = context.Todoitems.Where(src => src.Id == id).FirstOrDefault();
+            if (item == null)
+            {
+                throw new Exception("item not found");
+            }
+           
+            // return
+            return item;
+        }
+
+        public Todoitem Update(Todoitem item)
+        {
+           // update row
+           context.Update(item);
+
+            // save changes
+            context.SaveChanges();
+
+            // Return
+            return item;
         }
     }
 }
